34f27809b4bbcc35241a2e59b3bb0f7c43f4de0d
جهاد أحمد <gehad0ahmed28@gmail.com>
2024-02-14 21:13:05 +0200

    UI List

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Cube : MonoBehaviour
+{
+    
+}
