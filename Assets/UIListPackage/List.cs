c09e22c58674a481cb4d2ac97fe9a53a5e0714dd
جهاد أحمد <gehad0ahmed28@gmail.com>
2024-02-15 21:17:49 +0200

    Object rotation - Level2 Scenes Intergration - Ui edit

--- Assets/UIListPackage/List.cs
+++ Assets/UIListPackage/List.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 using UnityEngine.XR;
 
 public class List : MonoBehaviour
@@ -10,17 +11,30 @@ public class List : MonoBehaviour
     // public GameObject[] listofTicks;
     public GameObject preview;
     // public GameObject list;
-    private bool found = false; 
     private float timer = 0f;
+    
+    private TrackedPoseDriver trackedPoseDriver;
+
     private void Start()
     {
+        
+    }
+
+    private void Awake()
+    {
+        trackedPoseDriver = gameObject.transform.parent.GetComponent<TrackedPoseDriver>();
     }
 
     private void Update()
     {
         timer += Time.deltaTime;
-        if (timer >= 20)
+        if (timer is > 2 and < 20)
         {
+            trackedPoseDriver.trackingType = TrackedPoseDriver.TrackingType.RotationOnly;
+        }
+        else if (timer >= 20)
+        {
+            trackedPoseDriver.trackingType = TrackedPoseDriver.TrackingType.RotationAndPosition;
             gameObject.SetActive(false);
         }
     }
