3092a0e4200bf97b6653fd35343f15269eb182d6
جهاد أحمد <gehad0ahmed28@gmail.com>
2024-06-28 20:54:41 +0300

    Tutorial Scene

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Tutorial : MonoBehaviour
+{
+    public void Start()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    
+}
