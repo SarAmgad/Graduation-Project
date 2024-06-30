a2348819c9a4a71654a00abe6d56eacd742b0c95
جهاد أحمد <gehad0ahmed28@gmail.com>
2024-06-30 03:41:48 +0300

    Edit B button in tutorial

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -4,9 +4,28 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
+
 
 public class Tutorial : MonoBehaviour
 {
+    private InputData _inputData;
+
+    public GameObject menu;
+
+    void Start()
+    {
+        _inputData = GetComponent<InputData>();
+    }
+
+    void Update()
+    {
+        if (_inputData._rightController.TryGetFeatureValue(CommonUsages.secondaryButton, out bool Bbutton) && Bbutton)
+        {
+            menu.SetActive(true);
+        }
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
